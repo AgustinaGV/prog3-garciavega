3c433b21075dcd0017ddbdb6d462bdbfc7fafae9
AgustinaGV <agustinagarciavega@gmail.com>
2020-04-02 00:50:56 -0300

    Update TODO
    Ejercitación completa hasta el punto 8 (falta el 7). Modificado el main.js
    
    Agrego a la carpeta el main-Axel.js que son sus apuntes . Tienen algunas cosas más que los míos de la misma clase

--- clase2_ES6/assets/js/main-Axel.js
+++ clase2_ES6/assets/js/main-Axel.js
@@ -20,139 +20,110 @@
 
 // 1) Const y Let (Block Scope)
 
-let miVariable = 10;
-if(true){
-   let miVariable = 10;
-    console.log("variable 2", miVariable)
-}
-if(true){
-    let miVariable = 60;
-    console.log("variable 3", miVariable)
-}
-console.log("variable 1", miVariable)
-
-let miVariableLet = 0;
-console.log(miVariableLet);
-
-
-const miConst = 0;
-//miConst = 10;
-
-const miObjeto = {
-    nombre: 'Mike',
-    apellido: 'Morron',
-}
-
-// miObjeto = {
-//     nombre: 'Susan',
-//     apellido: 'Pere'
-// }
-
 const miArray = [1,2,3];
 let segundoArray = miArray;
 segundoArray.push(4);
 
-console.log("miArr", miArray);
-console.log("segd", segundoArray)
-
+console.log("miArr: ", miArray);
+console.log("segArr: ", segundoArray);
 
 //2) Arrow Functions
 
-function suma(num1, num2){
-    return num1 + num2;
+function suma(x,y)
+{
+    return x+y;
 }
 
-console.log("mi suma:", suma(2,4))
+console.log("mi suma: ", suma(2,4));
 
-const sumaArrow = (num1, num2) => {
-    return num1 + num2;
-}
+const sumaArrow = (n1, n2) => { return n1+n2;}
 
-console.log("mi suma Arrow:", sumaArrow(2,5))
+console.log("mi suma Arrow:", sumaArrow(2,5));
 
 const sumaArrow2 = (num1, num2) => num1 + num2;
 
-console.log("mi suma Arrow 2:", sumaArrow2(2,10))
+console.log("mi suma Arrow 2: ", sumaArrow2(2,10));
 
-const escribeMensaje = msg => console.log('mi mensaje es: ', msg);
+const escribeMensaje = msg => console.log("mi mensajes : ", msg);
 
-escribeMensaje('Hola mike');
+escribeMensaje('NICE, no lleva parentesis');
 
 
 //3) Modules (Imports and Exports)
+import { peliculasCopadas, cantidadPeliculas} from '../js/libreriaAmiga.js';
 
-import { peliculasCopadas, cantidadPeliculas } from '../js/libreriaAmiga.js';
-console.log('mis Pelis:',peliculasCopadas)
-console.log('cantidad de mis Pelis:',cantidadPeliculas(peliculasCopadas))
+console.log("Mis pelis: ", peliculasCopadas);
+console.log("cantidad de mis peliculas: ",cantidadPeliculas);
 
 //4) Template Literals
 
 var nombre = 'Mike';
-var saludo = 'Hola';
-var msg = 'buen dia';
+var saludo = "Hola";
+var msg = 'Buen día';
 
-var mensajeCompleto = saludo + " " + nombre + " " + msg;
+const funcTexto = () => 'hola';
+
+var mensajeCompleto = nombre + " " + saludo + " " + msg;
 
 console.log(mensajeCompleto);
 
 let nombreTL = 'Mike';
-let saludoTL = 'Hola';
-let msgTL = 'buen dia';
-
-const funcTexto = () => 'hola';
+let saludoTL = "Hola";
+let msgTL = 'Buen día';
 
 let mensajeCompletoTL = `${saludoTL} es un buen dia para ser ${nombreTL} y mi func texto es: ${funcTexto()}`;
-
-let miParrafo = `<p class="clase_parrafo">Hola soy 'el' Diego</p>`
-
-const $content = document.querySelector('#content');
+let miParrafo = "<p class='class_parrafo'>Hola, soy el 'el' Lean</p>";
+const $content = document.querySelector("#content");
 $content.innerHTML = miParrafo;
-
 console.log(mensajeCompletoTL);
 
-// `` -> '' -> ""
+// `` -> '' --> ""
 
-//5) Classes (Constructor, Super, Setters and Getters)
+//5)Classes (Constructor, Super, Setters and Getters)
 
-class Perro {
-    constructor(colorPelo, nombreDuenio, sexo, raza, tieneCollar){
+class Perro
+{
+    constructor(colorPelo, nombreDuenio, sexo, raza, tieneCollar)
+    {
         this.colorPelo = colorPelo;
         this.nombreDuenio = nombreDuenio;
+        this.colorPelo = colorPelo;
         this.sexo = sexo;
         this.raza = raza;
         this.tieneCollar = tieneCollar;
     }
 
-    ladrar(){
-        return `Hola!, Mi color de pelo es ${this.colorPelo} y soy de raza ${this.raza} y ${this.tieneCollar ? 'tengo': 'no tengo'} collar`
+    ladrar()
+    {
+        return `Mi color de pelo es ${this.colorPelo} y soy de raza ${this.raza} y ${this.tieneCollar ? 'tengo': 'no tengo'} collar`;
     }
 
 }
 
-const perro1 = new Perro('Negro', 'Josep', 'Hembra', 'Terrier', true);
-console.log(perro1.ladrar())
+const perro1 = new Perro('negro', 'Josep', 'M', 'Terrier', false);
+console.log(perro1.ladrar());
 
 //6) Default Parameters
-
-const division = (num1 = 10, num2 = 6) => {
-    if(num2 > 0){
+const division = (num1 = 0,num2 = 2 ) =>     {
+    if(num2 >0)
+    {
         return num1/num2;
-    }else{
+    }
+    else
+    {
         return false;
     }
-} 
+}
+
 console.log("mi resultado es: ", division(20));
 
+//7)The Spread Operator
 
-//7) The Spread Operator
-
-const numerosNegativos = [-100, -200, -450, -500];
-const numerosPositivos = [100, 300, 500, 720];
-
+const numerosNegativos = [-100,-200,-450,-500];
+const numerosPositivos = [100,300,500,720];
 const numerosReales = [...numerosNegativos, ...numerosPositivos, 1000];
 
-console.log("Numeros reales", numerosReales)
-
+console.log("numeros reales: ", numerosReales);
 
 //8) Destructuring
 
@@ -160,20 +131,16 @@ const clima = {
     unidad: 'Celsius',
     temperatura: 23,
     provincia: 'Buenos Aires',
-    pais: 'Argentina',
+    pais: 'Argentina'
 }
 
 //const temperatura = clima.temperatura;
 //const provincia = clima.provincia;
 
-const { unidad, temperatura: temp, temperatura } = clima 
+const { unidad, temperatura: temp, provincia, pais} = clima;
+console.log("la temp es ", temp);
 
-console.log("la temp es: ", temp)
-console.log("la temperatura es: ", temperatura)
-
-
-// 9) Rest Operator
-
+//9) Rest Operator
 const universidad = {
     name: 'Umai',
     website: 'https://umai.com',
@@ -181,12 +148,10 @@ const universidad = {
     director: "Wolko"
 }
 
-const { name, website, ...otrosDatos} = universidad;
-
-console.log("nombre", name);
-console.log("Web", website);
-console.log("otros datos", otrosDatos);
-
+const {name, website, ...otrosDatos} = universidad;
+console.log("nombre ", name);
+console.log("Sitio web: ", website);
+console.log("otros: ", otrosDatos);
 
 //10) map(), filter(), reduce() 
 
--- clase2_ES6/assets/js/main.js
+++ clase2_ES6/assets/js/main.js
@@ -42,15 +42,113 @@ const alumnos = [
     { nombre: 'Diego Salischiker', edad: 29 },
 ]
 
+console.log(alumnos);
 
 // 1. Obtener un array de strings con solo nombres de cada alumno usando .map()
+
+// Funcion declarada normalmente;
+/*function mostrarNombreAlumnos(alumnos) {
+    return alumnos.nombre;
+}*/
+
+// Arrow Function;
+const mostrarNombreAlumnos = (alumnos) => {
+    return alumnos.nombre;
+}
+
+// .map llama a la misma funcion para cada array del objeto alumnos, por lo que SIEMPRE necesita que se le pase un parametro;
+const nombreAlumnos = alumnos.map(mostrarNombreAlumnos);
+nombreAlumnos.toString();
+// Imprimo en pantalla rta ejercicio 1;
+console.log('1- Los nombres de lxs alumnxs son: ', nombreAlumnos);
+
 // 2. Obtener un array con aquellos alumnos mayores a 25 años usando .filter()
+
+// Funcion declarada normalmente;
+/*function filtrarAlumnosEdad (alumnos) {
+
+    return alumnos.edad > 25;
+}*/
+
+// Arrow Function;
+const filtrarAlumnosEdad = (alumnos) => {
+    return alumnos.edad > 25;
+}
+
+const alumnosMayores = alumnos.filter(filtrarAlumnosEdad);
+//Imprimo en pantalla rta ejercicio 2;
+console.log('2- Los alumnos mayores de 25 años son: ', alumnosMayores);
+
 // 3. Obtener un entero con la edad total de todos los alumnos usando .reduce() (Investigar: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/reduce)
+
+//Funcion declarada normalmente;
+/*function sumaEdades (total, alumno) {
+    return total + alumno.edad;
+}
+let sumaEdadAlumnos = alumnos.reduce(sumaEdades(total, alumno), 0);*/
+
+const sumaEdadAlumnos = alumnos.reduce(function (total, alumno) {
+    return total + alumno.edad;
+}, 0);
+// Imprimo en pantalla rta ejercicio 3;
+// Resultado esperado 23 + 32 + 20 + 19 + 22 + 24 + 19 + 33 + 27 + 29 = 248;
+console.log('3- La suma de las edades de todos los alumnos es igual a: ', sumaEdadAlumnos);
+
 // 4. Obtener en una constante la edad de "Franco Picco" usando .find() ( Investigar: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/find ) y destructuring del resultado. Investigar método includes
+
+// Includes;
+// da 'false' pero porque no estoy sabiendo como hacer para buscar en los objetos DENTRO del array 'alumnos';
+//console.log(alumnos.includes(33));
+
+//Funcion declarada normalmente;
+/*function buscarEdadFranco (alumnos) {
+    return alumnos.edad === 33;
+};*/
+
+//Arrow Function;
+const buscarEdadFranco = (alumnos) => {
+    return alumnos.edad === 33;
+};
+
+let [FrancoPicco] = [alumnos.find(buscarEdadFranco)];
+let [edadFrancoPicco] = [FrancoPicco.edad];
+console.log('4- Constante con datos de Franco Picco: ', FrancoPicco);
+// Imprimo en pantalla rta del ejercicio 4;
+console.log('4 bis- Bueno, solo con la edad: ', edadFrancoPicco);
+
 // 5. Obtener en una constante primer alumno del array de alumnos usando destructuring y posteriormente en otra constante su nombre también
+
+const [primerAlumno] = [alumnos[0]];
+// Imprimo en pantalla rta del ejercicio 5 (constante con todos los datos del primer alumno);
+console.log('5- Constante con datos del primer alumno: ', primerAlumno); 
+const [primerAlumnoNombre] = [primerAlumno.nombre];
+// Imprimo en pantalla rta bis del ejercicio 5 (constante que contiene solamente el nombre del primer alumno);
+console.log('5 bis- Constante con el nombre primer alumno: ', primerAlumnoNombre);
+
 // 6. Obtener un array con aquellos alumnos que empiezan con la letra "M", usando .filter()
+
+//Funcion declarada normalmente;
+/*function alumnosConM (alumnos) {
+    return alumnos.nombre.startsWith('M');
+}*/
+
+//Arrow Function;
+const alumnosConM = alumnos.filter((alumnos) => {
+    return alumnos.nombre.startsWith('M');
+
+});
+// Imprimo en pantalla rta del ejercicio 6;
+console.log('6- Los alumnos cuyos nombres empiezan con la letra "M" son:', alumnosConM);
+
 // 7. Obtener un array agregando una propiedad/key/atributo más a cada elemento usando .map()
+ 
+console.log('7- El array con la nueva propiedad:')
+
 // 8. Obtener a partir de la constante en 3, el promedio de edad del curso dividiendo la misma por el total de alumnos
+
+const promedioEdad = sumaEdadAlumnos/alumnos.length;
+// Imprimo en pantalla rta del ejercicio 8;
+console.log('8- Promedio de edad del curso: ', promedioEdad);
 
 
 //Async/Await
